c0d82b255ca17ff1fac4767e43a81dd28df05b42
rleukhin93 <leukhinroman@gmail.com>
2022-11-08 11:41:21 +0300

    update

--- js/script.js
+++ js/script.js
@@ -36,95 +36,322 @@ window.onload = function () {
     }
   })
 
-  //включаем анимацию счетчиков при прокрутке экрана до соот сетчика
-  const pageCounts = document.querySelectorAll(".number-js");
+  //включаем анимацию счетчиков при прокрутке экрана до соот графика
+  //прокрутили до 1-ого графика
+  const firstGrag = document.querySelector(".graf-js--1");
+  const firstGragCounts = firstGrag.querySelectorAll(".number-js");
+  const firstGragTextNums = firstGrag.querySelectorAll(".graf-text-num");
+  const collsFirst = firstGrag.querySelectorAll('.graf-text-month')
 
-  pageCounts.forEach(item => {
+  //условие на случай окна меньше 768px
+  //в этом случе нач анимации startFistGrafAnimation должно поменяться
+  // на document.querySelector('.mobile-start-first-graf-animation')
 
-    var numberTop = item.getBoundingClientRect().top
+  if ($(window).width() < 768) {
+    var startFistGrafAnimation = document.querySelector('.mobile-start-first-graf-animation').getBoundingClientRect().top;
+  }
+  else {
+    var startFistGrafAnimation = document.querySelector('.start-first-graf-animation').getBoundingClientRect().top;
+  }
+
+  firstGragCounts.forEach(item => {
     var start = +item.innerHTML
     var end = +item.dataset.max
     var speed = +item.dataset.speed
 
     window.addEventListener('scroll', function onScroll() {
-      if (window.pageYOffset > numberTop - window.innerHeight ) {
+
+      if (window.pageYOffset > startFistGrafAnimation - window.innerHeight) {
         this.removeEventListener('scroll', onScroll);
         var interval = setInterval(function () {
-          item.innerHTML = ++start;
+          collsFirst.forEach(item => { item.classList.add('animation-bt'); }) //заполняем столбцы
+          item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          firstGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
           if (start == end) {
             clearInterval(interval);
           }
-          // });
+        }, speed); // скорость прокрутки
+      }
+
+
+    });
+
+  });
+
+
+  //прокрутили до 2-ого графика
+  const secondGrag = document.querySelector(".graf-js--2");
+  const secondGragCounts = secondGrag.querySelectorAll(".number-js");
+  const secondGragTextNums = secondGrag.querySelectorAll(".graf-text-num");
+  const collsSecond = secondGrag.querySelectorAll('.graf-text-month')
+  const startSecondGrafAnimation = document.querySelector('.start-second-graf-animation').getBoundingClientRect().top;
+
+  secondGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startSecondGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          collsSecond.forEach(item => { item.classList.add('animation-bt'); }) //заполняем столбцы
+          item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          secondGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
+          if (start == end) {
+            clearInterval(interval);
+          }
         }, speed); // скорость прокрутки
       }
     });
 
   })
 
-  //включаем анимацию графиков при прокрутке экрана до соот графика
-  const svgGrafs = document.querySelectorAll(".graf-js");
-  
-  svgGrafs.forEach(item => {
-    var svgGrafNumbers = item.querySelectorAll(".number-js");
-    // console.log('svgGrafNumbers = ',svgGrafNumbers);
-    var numberTop = item.getBoundingClientRect().top;
-    const colls = item.querySelectorAll('.graf-text-month')
+  //прокрутили до 3-ого счетчика
+  const thirdGrag = document.querySelector(".number-js--3");
+  const startThirdGrafAnimation = document.querySelector('.start-third-graf-animation').getBoundingClientRect().top;
+
+  var start = +thirdGrag.innerHTML
+  var end = +thirdGrag.dataset.max
+  var speed = +thirdGrag.dataset.speed
+
+  window.addEventListener('scroll', function onScroll() {
+    if (window.pageYOffset > startThirdGrafAnimation - window.innerHeight) {
+      this.removeEventListener('scroll', onScroll);
+      var interval = setInterval(function () {
+        thirdGrag.innerHTML = ++start; // увеличиваем счетчик
+        if (start == end) {
+          clearInterval(interval);
+        }
+      }, speed); // скорость прокрутки
+    }
+  });
+
+  //прокрутили до 4-ого графика
+  const fourthGrag = document.querySelector(".graf-js--4");
+  const fourthGragCounts = fourthGrag.querySelectorAll(".number-js");
+  const fourthGragTextNums = fourthGrag.querySelectorAll(".graf-text-num");
+  const collsFourth = fourthGrag.querySelectorAll('.graf-text-month')
+  const startFourthGrafAnimation = document.querySelector('.start-fourth-graf-animation').getBoundingClientRect().top;
+
+  fourthGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
 
     window.addEventListener('scroll', function onScroll() {
-      if (window.pageYOffset > numberTop - window.innerHeight ) {
+      if (window.pageYOffset > startFourthGrafAnimation - window.innerHeight) {
         this.removeEventListener('scroll', onScroll);
         var interval = setInterval(function () {
+          collsFourth.forEach(item => { item.classList.add('animation-bt'); }) //заполняем столбцы
+          item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          fourthGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
 
-          console.log('до скролили до графика ');
-
-          //добавим animation-bt к соот столбикам(увеличим столбцы)
-          colls.forEach(item => { item.classList.add('animation-bt'); })
-
-
-          if (true) {// выходим из цикла
+          if (start == end) {
             clearInterval(interval);
           }
-        });
-      }
-    });
-  })
-
-  //включаем анимацию баров(report-bar-js ) при прокрутке экрана до соотбаров(report-bar-js )
-  const reportBars = document.querySelectorAll(".report-bar-js ");
-
-  reportBars.forEach(item => {
-    var numberTop = item.getBoundingClientRect().top;
-    // const colls = item.querySelectorAll('.graf-text-month')
-
-    window.addEventListener('scroll', function onScroll() {
-      if (window.pageYOffset > numberTop - window.innerHeight / 2) {
-        this.removeEventListener('scroll', onScroll);
-        var interval = setInterval(function () {
-          const animateDirection = item.dataset.direction
-
-          console.log('до скролили до бара item = ', item);
-          console.log('до скролили до бара animateDirection = ', item.dataset.direction);
-          // надо увеличить добавить animation-bt к соот столбикам
-          if (item.dataset.direction == 'lr') {
-
-            item.classList.add('animation-lr');
-
-          }
-          if (item.dataset.direction == 'rl') {
-
-            item.classList.add('animation-rl');
-          }
-
-
-          if (true) {// выходим из цикла
-            clearInterval(interval);
-          }
-        });
+        }, speed); // скорость прокрутки
       }
     });
 
   });
 
+  //прокрутили до 5-ого графика
+  const fifthGrag = document.querySelector(".graf-js--5");
+  const fifthGragCounts = fifthGrag.querySelectorAll(".number-js");
+  const fifthGragUnit = fifthGrag.querySelectorAll(".circle");
+  const fifthGragTestDiagrams = fifthGrag.querySelectorAll(".test-diagrams-items");
+  const startFifthGrafAnimation = document.querySelector('.start-fifth-graf-animation').getBoundingClientRect().top;
+
+  // console.log('fifthGragUnit = ', fifthGragUnit);
+
+  fifthGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startFifthGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          item.innerHTML = ++start; // увеличиваем счетчик
+
+          // убираю прозрачность у счетчиков
+          fifthGragUnit.forEach(item => { item.classList.add('unit'); })
+          fifthGragTestDiagrams.forEach(item => { item.classList.add('opacity-1'); })
+
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
+
+  //прокрутили до 6-ого блока
+  const sixthGrag = document.querySelector(".graf-js--6");
+  const sixthGragCounts = sixthGrag.querySelectorAll(".number-js");
+  const collsSixth = sixthGrag.querySelectorAll('.report-bar')
+  const startSixthGrafAnimation = document.querySelector('.start-sixth-graf-animation').getBoundingClientRect().top;
+
+  sixthGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startSixthGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          collsSixth.forEach(item => { item.classList.add('animation-lr'); }) //заполняем столбцы
+          item.innerHTML = ++start; // увеличиваем счетчик
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
+
+  //прокрутили до 7-ого блока
+  const seventhGrag = document.querySelector(".graf-js--7");
+  const seventhGragCounts = seventhGrag.querySelectorAll(".number-js");
+  const seventhGragTextNums = seventhGrag.querySelectorAll(".graf-text-num");
+  const collsSeventh = seventhGrag.querySelectorAll('.graf-text-month')
+
+  //условие на случай окна меньше 768px
+  //в этом случе нач анимации startFistGrafAnimation должно поменяться
+  // на document.querySelector('.mobile-start-first-graf-animation')
+
+  if ($(window).width() < 768) {
+    var startSeventhGrafAnimation = document.querySelector('.mobile-start-seventh-graf-animation').getBoundingClientRect().top;
+  }
+  else {
+    var startSeventhGrafAnimation = document.querySelector('.start-seventh-graf-animation').getBoundingClientRect().top;
+  }
+
+  seventhGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startSeventhGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          collsSeventh.forEach(item => { item.classList.add('animation-bt'); }) //заполняем столбцы
+          item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          seventhGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
+
+  //прокрутили до 8-ого блока
+  eighthGrag = document.querySelector(".graf-js--8");
+  const eighthGragCounts = eighthGrag.querySelectorAll(".graf-text");
+  const eighthGragTextNums = eighthGrag.querySelectorAll(".graf-text-num");
+  const collsEighth = eighthGrag.querySelectorAll('.graf-text-month')
+  const startEighthGrafAnimation = document.querySelector('.start-eighth-graf-animation').getBoundingClientRect().top;
+
+  eighthGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startEighthGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          collsEighth.forEach(item => { item.classList.add('animation-bt'); }) //заполняем столбцы
+          // item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          eighthGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
+
+  //прокрутили до 9-ого счетчика (77)
+  const ninthGrag = document.querySelector(".number-js--9");
+  const startNinthGrafAnimation = document.querySelector('.start-ninth-graf-animation').getBoundingClientRect().top;
+
+  var start9 = +ninthGrag.innerHTML
+  var end9 = +ninthGrag.dataset.max
+  var speed9 = +ninthGrag.dataset.speed
+
+  window.addEventListener('scroll', function onScroll() {
+    if (window.pageYOffset > startNinthGrafAnimation - window.innerHeight) {
+      this.removeEventListener('scroll', onScroll);
+      var interval = setInterval(function () {
+        ninthGrag.innerHTML = ++start9; // увеличиваем счетчик
+        if (start9 == end9) {
+          // if (ninthGrag.innerHTML >= end9) {
+          clearInterval(interval);
+        }
+      }, speed9); // скорость прокрутки
+    }
+  });
+
+
+  //прокрутили до 10-ого графика
+  const tenthGrag = document.querySelector(".graf-js--10");
+  const tenthGragCounts = tenthGrag.querySelectorAll(".number-js");
+  const tenthGragTextNums = tenthGrag.querySelectorAll(".text-bold");
+  const tenthGragBars = tenthGrag.querySelectorAll(".report-bar-js");
+  const startTenthGrafAnimation = document.querySelector('.start-tenth-graf-animation').getBoundingClientRect().top;
+
+  tenthGragCounts.forEach(item => {
+    var start = +item.innerHTML
+    var end = +item.dataset.max
+    var speed = +item.dataset.speed
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startTenthGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          item.innerHTML = ++start; // увеличиваем счетчик
+          // убираю прозрачность у счетчиков
+          tenthGragTextNums.forEach(item => { item.classList.add('opacity-1'); })
+
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
+
+  tenthGragBars.forEach(item => {
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > startTenthGrafAnimation - window.innerHeight) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function () {
+          item.classList.add('animation-rl');
+          if (true) {
+            clearInterval(interval);
+          }
+        }, speed); // скорость прокрутки
+      }
+    });
+
+  });
 
   //перeход по страницам(пунктам меню)
   //прокрутка до след пункта + фиксация соот пункта вверху стр 
@@ -184,9 +411,7 @@ window.onload = function () {
     });
   }
 
-
   window.addEventListener("scroll", navHighlighter);
-
 
   // make any elements animated by class
   // $(".first-screen__img").addClass("wow animate__fadeInUp");
@@ -208,43 +433,4 @@ window.onload = function () {
   $('.jumbotron-2').paroller();
   $('.jumbotron-3').paroller();
 
-
-  // var income = document.getElementById("income").getContext("2d");
-  // var barData = {
-  //   labels: ["", "", ""],
-  //   datasets: [
-  //     {
-  //       fillColor: "blue",
-  //       strokeColor: "blue",
-  //       data: [25, 56, 19]
-  //     },
-  //     {
-  //       fillColor: "green",
-  //       strokeColor: "green",
-  //       data: [24, 50, 25]
-  //     }
-
-  //   ]
-  // }
-  // new Chart(income).Bar(barData);
-
-
-  /* //  test-diagrams */
-  // let captionsList = document.querySelectorAll('.caption-item');
-  // let unitsList = document.querySelectorAll('.unit');
-
-  // captionsList.forEach(function (item, index) {
-  //   item.addEventListener('mouseover', function () {
-  //     unitsList[index].classList.add('hovered');
-  //   });
-
-  //   item.addEventListener('mouseout', function () {
-  //     unitsList[index].classList.remove('hovered');
-  //   });
-  // });
-
-
-
-
 }
-
